a257bb9fb0e1d8f3193ba4bf71d54a9d32e20c9b
Daan Zeeuwe <daan_zeeuwe@hotmail.com>
2017-06-08 11:00:28 +0200

    WNNAUTONOM-20 renamed variables to give more clarity and added a disturbance factor, for now on the twist topic /wind

--- src/NautonomousPropulsionSim.cpp
+++ src/NautonomousPropulsionSim.cpp
@@ -3,12 +3,15 @@
 #include <nav_msgs/Odometry.h>
 #include <nav_msgs/OccupancyGrid.h>
 
-geometry_msgs::Twist nautonomous_twist;
 nav_msgs::OccupancyGrid nautonomous_occupancy_grid;
 
-double vx = 0;
-double vy = 0;
-double vth = 0;
+double velocity_x = 0;
+double velocity_y = 0;
+double velocity_theta = 0;
+
+double disturbance_x = 0;
+double disturbance_y = 0;
+double disturbance_theta = 0;
 
 /**
  *\brief If the propulsion topic arrives, read message and set the velocity x, y, th for the robot.
@@ -16,18 +19,15 @@ double vth = 0;
  *\return 
  */
 void propulsionCallback(const geometry_msgs::Twist::ConstPtr& twist) {
-	nautonomous_twist.linear.x = twist->linear.x;
-	nautonomous_twist.linear.y = twist->linear.y;
-	nautonomous_twist.linear.z = twist->linear.z;
+	velocity_x = twist->linear.x;
+	velocity_y = twist->linear.y;
+	velocity_theta = twist->angular.z;
+}
 
-	nautonomous_twist.angular.x = twist->angular.x;
-	nautonomous_twist.angular.y = twist->angular.y;
-	nautonomous_twist.angular.z = twist->angular.z;
-
-	vx = twist->linear.x;
-	vy = twist->linear.y;
-	vth = twist->angular.z;
-
+void disturbanceCallback(const geometry_msgs::Twist::ConstPtr& twist) {
+	disturbance_x = twist->linear.x;
+	disturbance_y = twist->linear.y;
+	disturbance_theta = twist->angular.z;
 }
 
 /**
@@ -56,6 +56,8 @@ int main(int argc, char **argv) {
 	//Topics to subscribe to, multiplexed propulsion and the dimensions of the map.
 	ros::Subscriber propulsionSubscriber = n.subscribe(
 			"/multiplexed_propulsion", 10, propulsionCallback);
+	ros::Subscriber disturbanceSubscriber = n.subscribe(
+			"/wind", 10, disturbanceCallback);
 
 	ros::Subscriber mapSubscriber = n.subscribe("/map", 10, mapCallback);
 
@@ -64,9 +66,9 @@ int main(int argc, char **argv) {
 	tf::TransformBroadcaster odom_broadcaster;
 
 	//Position of the robot
-	double x = 0.0;//629267.0;
-	double y = 0.0;//5807159.0;
-	double th = 0.0;
+	double position_x = 0.0;//629267.0;
+	double position_y = 0.0;//5807159.0;
+	double theta = 0.0;
 
 	ros::Time current_time, last_time;
 	current_time = ros::Time::now();
@@ -80,39 +82,53 @@ int main(int argc, char **argv) {
 
 		/// compute odometry in a typical way given the velocities of the robot
 		double dt = (current_time - last_time).toSec();
-		double delta_x = (vx * cos(th) - vy * sin(th)) * dt;
-		double delta_y = (vx * sin(th) + vy * cos(th)) * dt;
-		double delta_th = vth * dt;
+		double delta_propulsion_x = (velocity_x * cos(theta) - velocity_y * sin(theta)) * dt;
+		double delta_propulsion_y = (velocity_x * sin(theta) + velocity_y * cos(theta)) * dt;
 
-		x += delta_x;
-		y += delta_y;
-		th += delta_th;
-		double degrees = (th * 180.0 / M_PI);
+		double delta_theta = velocity_theta * dt;
+
+		//only if the disturbance is measured from the nautonomous itself.
+		//double delta_disturbance_x = (disturbance_x * cos(theta) - disturbance_y * sin(theta)) * dt;
+		//double delta_disturbance_y = (disturbance_x * sin(theta) + disturbance_y * cos(theta)) * dt;
+
+		double delta_disturbance_x = disturbance_x * dt;
+		double delta_disturbance_y = disturbance_y * dt;
+
+		double delta_x = delta_propulsion_x + delta_disturbance_x;
+		double delta_y = delta_propulsion_y + delta_disturbance_y;
+
+		//ROS_INFO("delta (%4.2f, %4.2f | %4.2f)", delta_x, delta_y, delta_theta);
+		position_x += delta_x;
+		position_y += delta_y;
+		theta += delta_theta;
+		//ROS_INFO("pos (%4.2f, %4.2f | %4.2f)", position_x, position_y, theta);
+		double degrees = (theta * 180.0 / M_PI);
+
 		//x and y to index the occupancy grid map.
 		if(nautonomous_occupancy_grid.info.height){
-		int index_x = nautonomous_occupancy_grid.info.height
-				- (abs(x + nautonomous_occupancy_grid.info.origin.position.x)
-						/ nautonomous_occupancy_grid.info.resolution);
-		int index_y = ((y - nautonomous_occupancy_grid.info.origin.position.y)
-				/ nautonomous_occupancy_grid.info.resolution);
-		//ROS_INFO("(%4.2f, %4.2f | %4.2f [%4.2f]) is index (%d, %d)", x, y, th, degrees, index_x, index_y);
-        int index = nautonomous_occupancy_grid.info.height * index_y + index_x;
-        if (index >= 0 && index <= (nautonomous_occupancy_grid.info.height*nautonomous_occupancy_grid.info.width)) {
-			int value = nautonomous_occupancy_grid.data[index];
-			//ROS_INFO("index %d value %d", index, value);
+			int index_x = nautonomous_occupancy_grid.info.height
+					- (abs(position_x + nautonomous_occupancy_grid.info.origin.position.x)
+							/ nautonomous_occupancy_grid.info.resolution);
+			int index_y = ((position_y - nautonomous_occupancy_grid.info.origin.position.y)
+					/ nautonomous_occupancy_grid.info.resolution);
+			//ROS_INFO("(%4.2f, %4.2f | %4.2f [%4.2f]) is index (%d, %d)", x, y, th, degrees, index_x, index_y);
+			int index = nautonomous_occupancy_grid.info.height * index_y + index_x;
+			if (index >= 0 && index <= (nautonomous_occupancy_grid.info.height*nautonomous_occupancy_grid.info.width)) {
+				int value = nautonomous_occupancy_grid.data[index];
+				//ROS_INFO("index %d value %d", index, value);
 
-			if (value > 50) {
-				//reverse action
-				x -= delta_x;
-				y -= delta_y;
-				th -= delta_th;
+				if (value > 50) {
+					//undo position change
+					position_x -= delta_x;
+					position_y -= delta_y;
+					theta -= delta_theta;
+				}
 			}
-        }
-}
+		}
 
 		//since all odometry is 6DOF we'll need a quaternion created from yaw
 		geometry_msgs::Quaternion odom_quat = tf::createQuaternionMsgFromYaw(
-				th);
+				theta);
 
 		//first, we'll publish the transform over tf
 		geometry_msgs::TransformStamped odom_trans;
@@ -120,8 +136,8 @@ int main(int argc, char **argv) {
 		odom_trans.header.frame_id = "odom_combined";
 		odom_trans.child_frame_id = "base_link";
 
-		odom_trans.transform.translation.x = x;
-		odom_trans.transform.translation.y = y;
+		odom_trans.transform.translation.x = position_x;
+		odom_trans.transform.translation.y = position_y;
 		odom_trans.transform.translation.z = 0.0;
 		odom_trans.transform.rotation = odom_quat;
 
@@ -134,16 +150,16 @@ int main(int argc, char **argv) {
 		odom.header.frame_id = "odom_combined"; //odom_combined
 
 		//set the position
-		odom.pose.pose.position.x = x;
-		odom.pose.pose.position.y = y;
+		odom.pose.pose.position.x = position_x;
+		odom.pose.pose.position.y = position_y;
 		odom.pose.pose.position.z = 0.0;
 		odom.pose.pose.orientation = odom_quat;
 
 		//set the velocity
 		odom.child_frame_id = "base_link"; //base_link
-		odom.twist.twist.linear.x = vx;
-		odom.twist.twist.linear.y = vy;
-		odom.twist.twist.angular.z = vth;
+		odom.twist.twist.linear.x = velocity_x;
+		odom.twist.twist.linear.y = velocity_y;
+		odom.twist.twist.angular.z = velocity_theta;
 
 		//publish the message
 		odom_pub.publish(odom);
