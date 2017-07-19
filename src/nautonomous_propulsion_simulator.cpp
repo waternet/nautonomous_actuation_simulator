e9ec39b9f93d15492fbc5112730fbe946d15a912
Daan Zeeuwe <daan_zeeuwe@hotmail.com>
2017-07-19 20:44:28 +0200

    renamed comment

--- src/nautonomous_propulsion_simulator.cpp
+++ src/nautonomous_propulsion_simulator.cpp
@@ -161,7 +161,7 @@ int main(int argc, char **argv) {
 		//next, we'll publish the odometry message over ROS
 		nav_msgs::Odometry odom;
 		odom.header.stamp = current_time; 
-		odom.header.frame_id = "odom"; //odom_combined
+		odom.header.frame_id = "odom"; //odom
 
 		//set the position
 		odom.pose.pose.position.x = position_x;
